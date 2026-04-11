806002dd172993be82290c806aa5c1a274993a5a
Arief <arifcruyzel12@gmail.com>
2026-04-11 11:08:50 +0700

    Initial commit

---
+++ Pertemuan2/Latihan3.php
@@ -0,0 +1,14 @@
+<?php
+$p = 100; // panjang
+$l = 50;  // lebar
+$luas = $p * $l;
+?>
+<html>
+<head><title>Latihan 3</title></head>
+<body>
+<h2>Perhitungan Luas Persegi Panjang</h2>
+<p>Panjang = <?php echo $p; ?></p>
+<p>Lebar = <?php echo $l; ?></p>
+<p><b>Luas = <?php echo $luas; ?></b></p>
+</body>
+</html>
---
+++ Pertemuan6/Latihan1.php
@@ -0,0 +1,19 @@
+<?php
+$a = [];
+$a[0] = 1;
+$a[1] = 3;
+$a[2] = 5;
+
+$jumlah = count($a);
+echo "Jumlah array a = $jumlah <br>";
+
+// Array asosiatif
+$b = [];
+$b["buah"] = "semangka";
+$b["sayur"] = "wortel";
+$b["daging"] = "ayam";
+$b["utama"] = "nasi";
+
+$jumlah = count($b);
+echo "Jumlah array b = $jumlah <br>";
+?>
